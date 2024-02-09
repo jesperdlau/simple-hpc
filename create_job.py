19bcb60f3de3b3d2fe879a00c52311fe18c88121
jesperdlau <jesperdlau@gmail.com>
2024-02-09 17:38:52 +0100

    reworked the way arguments are parsed

---
+++ create_job.py
@@ -0,0 +1,39 @@
+import subprocess
+from omegaconf import OmegaConf
+import hydra
+
+# 
+@hydra.main(config_name="config.yaml", config_path="./", version_base="1.3")
+def main(config):
+    print(f"configuration: \n {OmegaConf.to_yaml(config)}")
+    
+    command = f"""bsub -q {config.bsub.queue} 
+                -J {config.bsub.name} 
+                -gpu {config.bsub.gpu_num}
+                -n {config.bsub.cpu_num}
+                -R "rusage[mem={config.bsub.cpu_mem}GB]"
+                -R "span[hosts=1]"
+                -W 24:00
+                -B 
+                -N 
+                -o lsf_logs/gpu_%J.out
+                -e lsf_logs/gpu_%J.err
+                -env "all" 
+                python3 my_program.py 
+                hyper.lr={config.hyper.lr} 
+                hyper.epoch={config.hyper.epochs}
+                hyper.batch_size={config.hyper.batch_size}
+                hyper.hidden_dim={config.hyper.hidden_dim}
+                """
+    command = command.replace("\n", " ")
+    command = " ".join(command.split())
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    
+    if error:
+        print(f"Error: {error}")
+    else:
+        print(f"Output: {output.decode('utf-8')}")
+        
+if __name__ == "__main__":
+    main()
